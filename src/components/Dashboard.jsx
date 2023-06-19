1beb1c5911049ebd00c3620862991cfad91838cc
Stefan Lemke <stban@posteo.de>
2023-06-19 15:38:15 +0200

    fixed authorization for createPost

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ThemeContext } from "../context/ThemeContext";
+import { useJwt } from "react-jwt";
 import {
   Button,
   CssBaseline,
@@ -14,9 +15,15 @@ import {
 
 export default function Dashboard() {
   const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
   const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const { token } = useContext(AuthContext);
   const { theme } = useContext(ThemeContext);
+  const { decodedToken } = useJwt(token);
+  const { login } = useContext(AuthContext);
+  const [error, setError] = useState(null);
 
   const disruptions = [
     {
@@ -38,6 +45,29 @@ export default function Dashboard() {
       value: "Others",
     },
   ];
+
+  const handleSubmit = async (e) => {
+    console.log("firing handleSubmit");
+    e.preventDefault();
+
+    const databody = {
+      title: title,
+      text: text,
+      user_id: decodedToken._id,
+    };
+
+    const response = await fetch("http://localhost:8080/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(databody),
+    });
+
+    setTitle("Please select category");
+    setText("");
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -87,6 +117,9 @@ export default function Dashboard() {
         <Container component="main" maxWidth="xs">
           <CssBaseline />
           <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
             sx={{
               marginTop: 8,
               display: "flex",
@@ -98,14 +131,30 @@ export default function Dashboard() {
             <Typography component="h1" variant="h5">
               Create a post
             </Typography>
-            <TextField fullWidth id="title" label="Title" select>
+            <TextField
+              fullWidth
+              id="title"
+              label="Title"
+              select
+              placeholder="Please select category"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            >
               {disruptions.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
                   {option.value}
                 </MenuItem>
               ))}
             </TextField>
-            <TextField fullWidth id="text" label="Text" multiline rows={10} />
+            <TextField
+              fullWidth
+              id="text"
+              label="Text"
+              multiline
+              rows={10}
+              onChange={(e) => setText(e.target.value)}
+              value={text}
+            />
             <Button
               type="submit"
               variant="contained"
